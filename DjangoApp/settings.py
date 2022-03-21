169e8523d9ef34b09a6421698fd6d3637b3701ce
Bryan Price <bprice89@gmail.com>
2022-03-21 14:06:40 -0400

    added db

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-1g*wiwa&78k1n0q*xe=s^j&pp7z$934z2z6tmaa33#upr7=cx4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['45.33.101.128']
+ALLOWED_HOSTS = ['45.33.101.128',
+                 '127.0.0.1',
+]
 
 
 # Application definition
